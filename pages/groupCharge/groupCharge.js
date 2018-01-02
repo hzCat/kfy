5b597a906182732894ec4bbdd4ee5c17652dd82f
hzCat <haizhoucat@qq.com>
2018-01-02 16:20:49 +0800

    group-page can get input value and consolo.log(value),and a little styles change in these group pages

--- pages/groupCharge/groupCharge.js
+++ pages/groupCharge/groupCharge.js
@@ -4,21 +4,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    step:0,
+    orderNumber: "",
+    money: "",
+    otherInfo: "",
+    phoneNumber: "",
+    pushInfo: {},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
-  },
 
+  },
+  // 获取汇款单号
+  getOrderNumber(e) {
+    console.log("汇款单号", e.detail.value)
+  },
+  // 获取金额
+  getMoney(e) {
+    console.log("金额", e.detail.value)
+  },
+  // 获取备注
+  getOtherInfo(e) {
+    console.log("备注", e.detail.value)
+  },
+  // 获取手机号码
+  getPhoneNumber(e) {
+    console.log("手机号码", e.detail.value)
+  },
+  pushAllInfo() {
+    console.log(this.data.pushInfo)
+  }
 })
--- pages/groupInfo/groupInfo.js
+++ pages/groupInfo/groupInfo.js
@@ -4,62 +4,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    step: 0,
+    companyName: "",
+    phoneNumber: "",
+    peopleName: "",
+    address: "",
+    orderNumber: "",
+    pushInfo:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
+  getCompanyName(e) {
+    console.log("company", e.detail.value)
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
+  getPhoneNumber(e) {
+    console.log("phonenumber", e.detail.value)
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
+  getPeopleName(e) {
+    console.log("peopleName", e.detail.value)
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
+  getAddress(e) {
+    console.log("address", e.detail.value)
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  getOrderNumber(e) {
+    console.log("orderNumber", e.detail.value)
+  },
+  pushAllInfo(){
+    console.log(this.data.pushInfo)
   }
 })
