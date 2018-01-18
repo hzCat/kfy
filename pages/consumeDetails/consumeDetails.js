28a34fc96c6edfd79b133feaa4904a865b616055
hzCat <haizhoucat@qq.com>
2018-01-18 20:13:11 +0800

    明细页样式修改,明细列表页跳转修改

--- pages/consumeDetails/consumeDetails.js
+++ pages/consumeDetails/consumeDetails.js
@@ -3,8 +3,8 @@ var navbar = require("../../utils/navbar.js");
 var http = require("../../utils/ajax.js");
 var storage = require("../../utils/storage.js");
 var modal = require("../../utils/modal.js");
+let app = getApp();
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -13,62 +13,78 @@ Page({
     third: "",
     header: {},
     pattern: "",
+    cardType: "VIP"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
-    console.log(options)
-    storage.gets("3rd_session")
-      .then(function (res) {
-        that.setData({
-          pattern: options.type,
-          third: res.data,
-          header: {
-            '_yzsaas_token': res.data,
-            "content-type": "application/x-www-form-urlencoded"
-          }
-        })
-        var url = "/vip/getVipCardRecordDetail";
-        var method = "GET";
-        var data = {
-          recordType: options.type,
-          recordId: options.id
-        }
-        var header = that.data.header
-        http.ajax(url, method, data, header)
-          .then(function (res) {
-            console.log(res.data.data)
-            if (res.data.data.consumeRecordDetail) {
-              that.setData({
-                detail: res.data.data.consumeRecordDetail
-              })
-            }
-            else if (res.data.data.rechargeRecordDetail) {
-              that.setData({
-                detail: res.data.data.rechargeRecordDetail
-              })
-            }
-          })
-          .catch((err) => {
-            modal.modal("提示", "未能获取详情,请返回重试")
-          })
+    console.log(options);
+    // storage.gets("3rd_session").then(function(res) {
+    that.setData({
+      pattern: options.type,
+      cardType:options.cardType
+      // third: res.data,
+      // header: {
+      //   _yzsaas_token: res.data,
+      //   "content-type": "application/x-www-form-urlencoded"
+      // }
+    });
+
+    let url = null;
+
+    if (options.type == "CONSUME_RECORD") {
+      // 消费
+      url = "/vip/getVipCardConsumeRecordDetail";
+    } else if (options.type == "RECHARGE_RECORD") {
+      // 充值
+      url = "/vip/getVipCardRechargeRecordDetail";
+    }
+
+    // var url = "/vip/getVipCardRecordDetail";
+    var method = "GET";
+    var data = {
+      // recordType: options.type,
+      recordId: options.id
+    };
+    var header = app.globalData.header;
+    http
+      .ajax(url, method, data, header)
+      .then(function(res) {
+        console.log(res.data.data);
+        if (res.data.data) {
+          let obj = res.data.data;
+          that.setData({
+            detail: obj,
+          });
+        } 
+        // else if (res.data.data.rechargeRecordDetail) {
+        //   let obj = res.data.data.rechargeRecordDetail;
+        //   that.setData({
+        //     detail: obj,
+        //     cardType: obj.vipScope
+        //   });
+        // }
       })
+      .catch(err => {
+        modal.modal("提示", "未能获取详情,请返回重试");
+      });
+    // });
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     // navbar.title("消费明细")
   },
 
   jump(e) {
-    console.log(e)
+    console.log(e);
     var pattern = e.currentTarget.dataset.pattern;
     var jumpto = e.currentTarget.dataset.jump;
-    jump.jump(pattern, jumpto)
+    jump.jump(pattern, jumpto);
   }
-})
+});
